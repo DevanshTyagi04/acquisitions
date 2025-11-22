c157d8b5577ea94c5f3d2860a79b7f71df341006
DevanshTyagi04 <devansh.tyagi04@gmail.com>
2025-11-23 02:27:49 +0530

    Fix Docker Github Action and linting

--- src/app.js
+++ src/app.js
@@ -15,34 +15,36 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('combined', {stream: {write: (message) => logger.info(message.trim())}}));
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
 
 app.use(cookieParser());
 
 app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
-  logger.info('Hello from Acquisitions!')
+  logger.info('Hello from Acquisitions!');
   res.status(200).send('Hello from Acquisitions!');
 });
 
 app.get('/health', (req, res) => {
-  res
-    .status(200)
-    .json({
-      status: 'OK',
-      timestamp: new Date().toISOString(),
-      uptime: process.uptime(),
-    });
+  res.status(200).json({
+    status: 'OK',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
 });
 
-app.get('/api',(req,res) => {
-    res.status(200).json({message: 'Acquisitions API is running!' });
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'Acquisitions API is running!' });
 });
 
-app.use('/api/auth',authRoutes);
+app.use('/api/auth', authRoutes);
 
-app.use('/api/users',usersRoutes);
+app.use('/api/users', usersRoutes);
 
 app.use((req, res) => {
   res.status(404).json({ error: 'Route not found' });
--- src/config/database.js
+++ src/config/database.js
@@ -14,4 +14,4 @@ const sql = neon(process.env.DATABASE_URL);
 
 const db = drizzle(sql);
 
-export { db,sql };
+export { db, sql };
