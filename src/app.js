49a4eab0c5ceea148941612b6407c2aa51b5c2ae
DevanshTyagi04 <devansh.tyagi04@gmail.com>
2025-11-22 23:22:34 +0530

    Secure our API with Arcjet

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.routes.js';
-import { timestamp } from 'drizzle-orm/gel-core';
+import securityMiddleware from '#middleware/security.middleware.js';
 
 const app = express();
 
@@ -16,7 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('combined', {stream: {write: (message) => logger.info(message.trim())}}));
 
-app.use(cookieParser())
+app.use(cookieParser());
+
+app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions!')
